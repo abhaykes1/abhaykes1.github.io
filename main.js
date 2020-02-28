0e098c0356777afb2f8cd43557921dd03fd53697
abhaykes1 <abhaykesharwani40@gmail.com>
2020-02-29 00:02:45 +0530

    updated project section

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 console.log(' i am here')
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    M.AutoInit();
+    var options = {
+      fullWidth: true, 
+      indicators: true,
+  };
+
+    var elem1 = document.querySelectorAll('.sidenav');
+    var elem2 = document.querySelector('.carousel');
+
+    var instances1 = M.Sidenav.init(elem1);
+    var instances2 = M.Carousel.init(elem, options);
+    instances2.pressed = false;
+    M.autoinit();
   });
