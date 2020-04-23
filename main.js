087f9027c27195d6f59f7bb7174d99f1d55146c4
abhaykes1 <abhaykesharwani40@gmail.com>
2020-04-24 01:38:25 +0530

    basic structure

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
       fullWidth: true, 
       indicators: true,
   };
+  var options2 = {
+    toolbarEnabled: true
+  }
 
     var elem1 = document.querySelectorAll('.sidenav');
     var elem2 = document.querySelector('.carousel');
+    var elem3 = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elem3, options2);
 
     var instances1 = M.Sidenav.init(elem1);
     var instances2 = M.Carousel.init(elem, options);
     instances2.pressed = false;
-    M.autoinit();
-  });
+    M.AutoInit();
+});
